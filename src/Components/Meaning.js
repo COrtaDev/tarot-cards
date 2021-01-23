081b4594d9381a3602bd3c6850508aff77da3a20
Conrad <conradorta@gmail.com>
2021-01-23 13:30:02 -0500

    gets info

--- src/Components/Meaning.js
+++ src/Components/Meaning.js
@@ -13,6 +13,14 @@ const Meaning = (props) => {
             }
             return;
         }
+        async function getFandomInfo() {
+            const page = await wiki({
+                apiUrl: 'https://tarot.fandom.com/api.php?',
+                origin: null
+            }).page('The_Magician')
+            console.log(page);
+        };
+        getFandomInfo()
         async function getInfo() {
             const page = await wiki().page(props.search);
             const content = await page.sections();
@@ -20,26 +28,39 @@ const Meaning = (props) => {
             setInfo(content);
         };
         getInfo();
-
     });
-
+    const titlesToAvoid = [
+        "History", "Bibliography", "References", "Alternative decks", "Other versions", "In other decks",
+        "External links", "See also", "Sources", "In other media", "Further reading",
+        "In art", "Notes", "Citations", "In popular culture", "Extras",
+    ];
     function handleInfo(info) {
-        // console.log(info)
-        //this breaks a lot. will need to focus here.
-        const text = [];
-        for (let i = 0; i < info.length; i++) {
-            // console.log(info[i].title.toLowerCase().includes('meanings'))
-            if (info[i].title.toLowerCase().includes('meanings')) {
-                text.push(info[i].content)
+        console.log("All Info: ", info)
+        const titles = info.map(section => section.title);
+        let text = info.map(section => {
+            if (!titlesToAvoid.includes(section.title)) {
+                if (!section.content && section.items) {
+                    console.log("Subsections: ", section.items)
+                    let subectionContent = section.items.map(subsection => subsection.content)
+                    console.log("Subsection Content: ", subectionContent);
+                    return [...subectionContent];
+                } else if (section.content && section.items) {
+                    console.log("Subsections: ", section.items)
+                    let subectionContent = section.items.map(subsection => subsection.content)
+                    console.log("Subsection Content: ", subectionContent);
+                    return [section.content, ...subectionContent,];
+                }
+                return section.content
             }
-
-        }
+        });
+        text = text.filter(content => content).flat();
+        console.log("Titles: ", titles);
         setText(text);
     };
-    console.log(text)
-    const cardText = text.map(text => {
+    console.log("Text: ", text)
+    const cardText = text.map((text, i) => {
         return (
-            <p>
+            <p key={i}>
                 {text}
             </p>
         );
---
+++ src/Helpers/getInfo.js
@@ -0,0 +1,9 @@
+import wiki from 'wikijs';
+
+export async function getFandomInfo() {
+    const page = await wiki({
+        apiUrl: 'https://tarot.fandom.com',
+        // origin: null
+    })
+    console.log(page);
+};
