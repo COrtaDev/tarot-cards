c20d4d599da0b916b923e5c6e78d4b7034813d30
Conrad <conradorta@gmail.com>
2021-01-29 21:56:58 -0500

    Expanded Classes and redefined params

---
+++ api/cards/card.py
@@ -0,0 +1,9 @@
+# card.py
+"""Card class that represents a generic card to be extended later"""
+
+
+class Card:
+    def __init__(self, deck):
+        """Initialize a Card with a name and deck."""
+        # self._name = name
+        self._deck = deck
--- api/cards/helper.py
+++ api/cards/helper.py
@@ -113,3 +113,8 @@ major = [
         "name": "The World"
     }
 ]
+otherhelper = ["The Fool", "The Magician", "The High Priestess",
+               "The Empress", "The Emperor", "The Hierophant", "The Lovers",
+               "The Chariot", "Justice", "The Hermit", "Wheel of Fortune",
+               "Strength", "The Hanged Man", "Death", "Temperance", "The Devil",
+               "The Tower", "The Star", "The Moon", "The Sun", "Judgement",  "The World"]
